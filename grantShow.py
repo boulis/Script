ab39108797cbbf2a65a776431053151488a68e94
Tnargwoxow <tnargwoxow@gmail.com>
2013-08-19 12:40:24 +1000

    Same

--- grantShow.py
+++ grantShow.py
@@ -179,7 +179,7 @@ class Show:
 		cdict['CommandID'] = 'MyCommandID'
 		response = self.manager.send_action(cdict)
 		print datetime.now(), "Playing audio file", filename, "to", actorName, ". Start response:", response
-        if response = 'Error'
+        if response == 'Error'
             response = self.manager.originate('SIP/didlogic-trunk/'+ phone, caller_id=actorName, async=True, exten='callwait', context='testcall', priority='1')
 		    print datetime.now(), 'Originating call to', actorName, phone, 'Response:', response
 
