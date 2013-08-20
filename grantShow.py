621cd10961cbe3a84e1b4f6b02d90ed4d8f7ba59
Tnargwoxow <tnargwoxow@gmail.com>
2013-08-20 18:24:02 +1000

    Thanassis update 20/08/2013

--- grantShow.py
+++ grantShow.py
@@ -198,7 +198,7 @@ class Show:
 		'''
 		cdict = {'Action':'AGI'}
 		cdict['Channel'] = self.channel[actorName]
-		cdict['Command'] = 'EXEC Playback ' + filename
+		cdict['Command'] = 'EXEC Playback ' + directory + filename
 		cdict['CommandID'] = 'MyCommandID'
 		response = self.manager.send_action(cdict)
 		print datetime.now(), "Playing audio file", filename, "to", actorName, ". Start response:", response
