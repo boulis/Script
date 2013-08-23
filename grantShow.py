d72875165a259bd8153ec137f19a776dc7ecb72a
Tnargwoxow <tnargwoxow@gmail.com>
2013-08-23 12:30:20 +1000

    Test background audio durign instructions

--- grantShow.py
+++ grantShow.py
@@ -269,7 +269,7 @@ class Show:
 			actorName = self.actor[event.headers['Uniqueid']]
 			# store the pressed digit, so that other threads can find it
 			self.pressedDTMF[actorName] = int(event.headers['Digit'])
-
+			self.playback('press1', actorName)
 			# notify the thread waiting for this by setting/trigering the right event
 			# if the event is not there, or is already set, then a thread is not waiting for it
 			if actorName in self.eventsDTMF and not self.eventsDTMF[actorName].is_set():
