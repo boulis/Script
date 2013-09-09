7abd4f24c9c62402720b467c84b8b27759bb439c
Tnargwoxow <tnargwoxow@gmail.com>
2013-09-09 19:51:01 +1000

    Monday preview show

--- grantShow.py
+++ grantShow.py
@@ -177,8 +177,8 @@ show.whenReconnected = '/audio/reconnected'
 show.thankyou = '/audio/thankyou'
 
 # the phones that we can call from to begin the main show. Add as many as you like
-triggerPhones = ['61413817002']
-show.collectPhones(triggerPhones)
+#triggerPhones = ['61413817002']
+#show.collectPhones(triggerPhones)
 # collect phones, you can also add an optional maximum delay in case no call from a trigger phone is made
 # we have collected phones. begin the show#
 show.begin(['61411111485', '61438871792', '61438790492', '61431629015', '61402337737', '61402029053'])
