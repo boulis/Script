e921df6836a246b14bc70a1690af25f2266d6d5d
Tnargwoxow <tnargwoxow@gmail.com>
2013-09-10 15:52:23 +1000

    Testing the press 1 instructions again

--- grantShow.py
+++ grantShow.py
@@ -175,8 +175,8 @@ show.whenReconnected = '/audio/reconnected'
 show.thankyou = '/audio/thankyou'
 
 # the phones that we can call from to begin the main show. Add as many as you like
-#triggerPhones = ['']
-#show.collectPhones(triggerPhones)
+triggerPhones = ['']
+show.collectPhones(triggerPhones)
 # collect phones, you can also add an optional maximum delay in case no call from a trigger phone is made
 # we have collected phones. begin the show#
 #show.begin()
