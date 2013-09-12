b9c0e9fe0be669a96b9606c24785aa9b1443a2ff
Tnargwoxow <tnargwoxow@gmail.com>
2013-09-12 10:11:15 +1000

    Trying to replicate errors + Trying a better way to recover from errors

--- debugger.py
+++ debugger.py
@@ -195,5 +195,5 @@ show.thankyou = '/audio/thankyou'
 #show.collectPhones(triggerPhones)
 # collect phones, you can also add an optional maximum delay in case no call from a trigger phone is made
 # we have collected phones. begin the show#
-show.begin([('61413817002', 'Andrew')])
+show.begin(['61413817002', 'Andrew'])
 #show.begin(['61413817002'])
